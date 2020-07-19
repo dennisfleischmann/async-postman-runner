fd12ab9329c793c73f088586840f415b000a5c08
Dennis <dennis.fleischmann@hotmail.com>
2020-07-19 16:59:39 +0200

    Added customConfig feature

--- utils.js
+++ utils.js
@@ -1,12 +1,19 @@
 const newman = require('newman'); // require newman in your project
 
-const  executeAsync = (collection)  => new Promise((resolve,reject) =>  {
+const  executeAsync = ({collection, customConfig})  => new Promise((resolve,reject) =>  {
 
-    console.log('repare');
-    newman.run({
+    const defaultConfig = {
         collection,
         reporters: ['cli', 'html'],
-    }, function (err) {
+    };
+
+    let config = defaultConfig;
+
+    if(customConfig) {
+        config = customConfig;
+    }
+    console.log('repare');
+    newman.run(config, function (err) {
         if (err) { reject(err) }
         resolve('collection run complete!');
     });
